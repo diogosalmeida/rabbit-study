7896183908a97c12dac19b1a63c71db11fe5e3c1
diogosalmeida <diogomean@gmail.com>
2017-12-28 19:12:50 -0200

    Exemplo de produce rabbitmq

---
+++ src/main/java/com/example/demo/Runner.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final ConfigurableApplicationContext context;
+
+	public Runner(RabbitTemplate rabbitTemplate, ConfigurableApplicationContext context) {
+		this.rabbitTemplate = rabbitTemplate;
+		this.context = context;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Sending message Pedro...");
+		rabbitTemplate.convertAndSend(Application.queueName, "Hello from RabbitMQ!");
+		context.close();
+	}
+
+}
